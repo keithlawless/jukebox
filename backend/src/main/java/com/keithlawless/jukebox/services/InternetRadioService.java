fa92a55637195e0761e463814a9da3e7ebf76aa0
Keith Lawless <keithlawless@gmail.com>
2021-03-28 18:23:43 -0400

    Fixed bug where stations.json couldn't be read when bundled in a jar file

--- backend/src/main/java/com/keithlawless/jukebox/services/InternetRadioService.java
+++ backend/src/main/java/com/keithlawless/jukebox/services/InternetRadioService.java
@@ -2,9 +2,12 @@ package com.keithlawless.jukebox.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.keithlawless.jukebox.entity.RadioStations;
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
@@ -14,7 +17,7 @@ import java.util.logging.Logger;
 @Service
 public class InternetRadioService {
     private static final Logger logger = Logger.getLogger(InternetRadioService.class.getName());
-    private static String dataFile = "data/stations.json";
+    private static String dataFile = "/data/stations.json";
 
     public RadioStations getStations() {
         RadioStations stations = new RadioStations();
@@ -34,9 +37,26 @@ public class InternetRadioService {
         return stations;
 
     }
-    private static String readFileFromResources(String filename) throws URISyntaxException, IOException {
-        URL resource = InternetRadioService.class.getClassLoader().getResource(filename);
-        byte[] bytes = Files.readAllBytes(Paths.get(resource.toURI()));
-        return new String(bytes);
+
+    private String readFileFromResources(String filename) throws URISyntaxException, IOException {
+        String dataFilePath = dataFilePath();
+        InputStream in = new URL(dataFilePath).openStream();
+        return new String(IOUtils.toByteArray(in));
+    }
+
+    private String dataFilePath() {
+        ClassPathResource dataFileResource = new ClassPathResource(dataFile);
+        try {
+            URL url = dataFileResource.getURL();
+            return url.toURI().toASCIIString();
+        }
+        catch(IOException ioe) {
+            logger.info("IOException in dataFilePath: " + ioe.toString());
+        }
+        catch(URISyntaxException e) {
+            logger.info("URISyntaxException in dataFilePath: " + e.toString());
+        }
+
+        return "";
     }
 }
