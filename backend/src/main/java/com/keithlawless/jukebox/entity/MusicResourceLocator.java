241080f1a9a7d5126125d15b09a490441052014f
keith <keith@keithlawless.com>
2026-02-14 17:56:10 -0500

    Accept URL encoded mrl when queueing songs

--- backend/src/main/java/com/keithlawless/jukebox/entity/MusicResourceLocator.java
+++ backend/src/main/java/com/keithlawless/jukebox/entity/MusicResourceLocator.java
@@ -1,8 +1,14 @@
 package com.keithlawless.jukebox.entity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import java.io.Serializable;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 
 /**
  * Represents a media resource location with its MRL (Media Resource Locator)
@@ -12,6 +18,7 @@ public class MusicResourceLocator implements Serializable {
 
     @Schema(description = "Media Resource Locator (MRL) string that identifies the media resource",
             example = "file:///music/queen/bohemian_rhapsody.mp3")
+    @JsonDeserialize(using = UrlDecodedStringDeserializer.class)
     private String mrl;
 
     public MusicResourceLocator() {}
@@ -27,4 +34,19 @@ public class MusicResourceLocator implements Serializable {
         this.mrl = mrl;
     }
 
+}
+
+class UrlDecodedStringDeserializer extends JsonDeserializer<String> {
+    @Override
+    public String deserialize(JsonParser p, DeserializationContext ctxt) throws java.io.IOException {
+        String value = p.getValueAsString();
+        if (value == null) {
+            return null;
+        }
+        try {
+            return URLDecoder.decode(value, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            return value;
+        }
+    }
 }
